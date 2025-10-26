b34d8ad0284d653dee3845a58c4168030db2d6b5
whitewolf18 <169000698+whitewolf18@users.noreply.github.com>
2025-10-26 17:04:36 +0200

    update

---
+++ Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this is for testing damage will be here
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        
+        healthBar.SetHealth(currentHealth);
+    }
+}
